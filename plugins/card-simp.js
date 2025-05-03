6a39e2381d147b1478cef62261cdd6765143f32e
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-05-02 21:28:42 -0500

    Update card-simp.js

--- plugins/card-simp.js
+++ plugins/card-simp.js
@@ -1,11 +1,21 @@
-const handler = async (m, {conn}) => {
-  const who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
-  conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/simpcard', {
-    avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-  }), 'error.png', '*¡WTF ERES SIMP* 😆', m);
-};
-handler.help = ['simp']
+const handler = async (m, { conn }) => {
+  try {
+    const who = m.quoted?.sender || m.mentionedJid?.[0] || m.fromMe ? conn.user.jid : m.sender
+
+    const avatar = await conn.profilePictureUrl(who, 'image')
+      .catch(() => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
+
+    const url = `https://some-random-api.com/canvas/simpcard?avatar=${encodeURIComponent(avatar)}`
+
+    await conn.sendFile(m.chat, url, 'simp.png', '*¡WTF ERES SIMP!* 😆', m)
+  } catch (e) {
+    console.error('[ERROR SIMPCARD]', e)
+    conn.reply(m.chat, '*❌ Ocurrió un error al generar la tarjeta simp.*', m)
+  }
+}
+
+handler.help = ['simp', 'simpcard']
 handler.tags = ['tools']
 handler.command = ['simp', 'simpcard']
 handler.register = true
-export default handler;
+export default handler
