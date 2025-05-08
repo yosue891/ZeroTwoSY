740217646829177055d7b43d970575e8c33519d7
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-05-07 21:55:39 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ import moment from 'moment-timezone'
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
-global.botNumber = '' //Ejemplo: 51921826291
+global.botNumber = '51921826291' //Ejemplo: 51921826291
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
