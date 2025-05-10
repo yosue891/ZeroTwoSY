f0e17ace2525a7230e78ab8cb88687c308b55443
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-05-10 00:03:19 -0500

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -11,8 +11,8 @@ let handler = async (m, { conn }) => {
   let name = await conn.getName(who);
 
   await sendContactArray(conn, m.chat, [
-    [`${suittag}`, `ᰔᩚ Propietario`, botname, `❀ No Hacer Spam`, correo, `⊹˚• Venezuela •˚⊹`, md, bio],
-    [`${conn.user.jid.split('@')[0]}`, `✦ Es Un Bot`, packname, dev, correo, `Sabra Dios 🫏`, channel, biobot]
+    [`${suittag}`, `Ayudante`, botname, `❀ No Hacer Spam`, correo, `⊹˚• Venezuela •˚⊹`, md, bio],
+    [`${conn.user.jid.split('@')[0]}`, `✦ Es Un Bot`, packname, dev, correo, `El Enamorado Jeje <3`, channel, biobot]
   ], m);
 }
 
