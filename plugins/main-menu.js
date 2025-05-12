90790ffe6eca2d19698728f223353a6a5c2b9ccb
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-05-12 11:14:14 -0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -398,3 +398,16 @@ let handler = async (m, { conn, args }) => {
     }
   }, { quoted: m })
 }
+
+handler.help = ['menu']  
+handler.tags = ['main']  
+handler.command = ['menu', 'menú', 'help', 'ayuda']  
+  
+export default handler  
+  
+function clockString(ms) {  
+  let seconds = Math.floor((ms / 1000) % 60)  
+  let minutes = Math.floor((ms / (1000 * 60)) % 60)  
+  let hours = Math.floor((ms / (1000 * 60 * 60)) % 24)  
+  return `${hours}h ${minutes}m ${seconds}s`  
+}
