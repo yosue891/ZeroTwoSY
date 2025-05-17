3c8b5e3a125d773345047269855b5deda676cc75
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-05-17 16:39:45 -0500

    Create chistoso.js

---
+++ chistoso.js
@@ -0,0 +1,22 @@
+const chistes = [
+  "¿Por qué los pájaros vuelan hacia el sur en invierno? ¡Porque caminar es demasiado lejos! (≧◡≦)",
+  "¿Qué le dice un semáforo a otro? ¡No me mires, me estoy cambiando! ♡",
+  "¿Cuál es el pez más presumido? El pez-ume. (⌒▽⌒)",
+  "¿Qué hace una abeja en el gimnasio? ¡Zum-ba! >w<",
+  "¿Por qué los fantasmas son malos mentirosos? ¡Porque se les ve transparentes! (o˘◡˘o)",
+];
+
+const handler = async (m, { conn }) => {
+  const texto = m.text || '';
+  if (/chistoso/i.test(texto)) {
+    const chisteAleatorio = chistes[Math.floor(Math.random() * chistes.length)];
+    await conn.reply(m.chat, chisteAleatorio, m);
+  }
+};
+
+handler.help = ['chistoso'];
+handler.tags = ['fun'];
+handler.command = ['chistoso'];
+handler.register = true;
+
+export default handler;
