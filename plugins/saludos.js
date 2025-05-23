5ad20dce79edc337b9e971081b2af14328c7fa7b
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-05-23 14:01:34 -0500

    Create saludos.js

---
+++ plugins/saludos.js
@@ -0,0 +1,19 @@
+const handler = async (m, { conn }) => {
+  const texto = m.text || '';
+
+  if (/saludoespecial/i.test(texto)) {
+    const saludoPersonalizado = `
+¡Un saludo súper especial para:! (≧◡≦)
+\n✨ Persona 1: Sofia ✨\n✨ Persona 2: Angel ✨\n✨ Persona 3: Dayana ✨\n✨ Persona 4: Genesis ✨
+\nQue tengan un día lleno de alegría y código genial! ♡`;
+
+    await conn.reply(m.chat, saludoPersonalizado, m);
+  }
+};
+
+handler.help = ['saludoespecial'];
+handler.tags = ['fun'];
+handler.command = ['saludoespecial']; // Aca MayCode es para cuando alguien diga saludoespecial el Bot responda
+handler.register = true;
+
+export default handler;
