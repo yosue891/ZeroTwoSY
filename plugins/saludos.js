56e7d3c97ee8312f41484f216dc1b9e6c2f95064
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-05-23 14:10:48 -0500

    Update saludos.js

--- plugins/saludos.js
+++ plugins/saludos.js
@@ -4,7 +4,7 @@ const handler = async (m, { conn }) => {
   if (/saludoespecial/i.test(texto)) {
     const saludoPersonalizado = `
 ¡Un saludo súper especial para:! (≧◡≦)
-\n✨ Persona 1: Sofia ✨\n✨ Persona 2: Angel ✨\n✨ Persona 3: Dayana ✨\n✨ Persona 4: Genesis ✨
+\n✨ Persona 1: Sofia ✨\n✨ Persona 2: Angel ✨\n✨ Persona 3: Dayana ✨\n✨ Persona 4: Genesis ✨\n✨ Persona 5: Abram Insano ✨
 \nQue tengan un día lleno de alegría y código genial! ♡`;
 
     await conn.reply(m.chat, saludoPersonalizado, m);
