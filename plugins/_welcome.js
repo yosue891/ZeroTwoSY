fc3aac016a6a62f7f94ee032bffd5566ad02ab06
yosue891 <yosueortega630@gmail.com>
2025-05-29 16:45:26 -0400

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -3,8 +3,8 @@ import fetch from 'node-fetch';
 export async function before(m, { conn, participants, groupMetadata }) {
   if (!m.messageStubType || !m.isGroup) return true;
 
-  let vn = 'https://files.catbox.moe/pazabz.m4a';
-  let vn2 = 'https://files.catbox.moe/t3j0sv.m4a';
+  let vn = 'https://files.catbox.moe/61g0ok.mp3';
+  let vn2 = 'https://files.catbox.moe/rzwj4z.mp3';
   let chat = global.db.data.chats[m.chat];
   const getMentionedJid = () => {
     return m.messageStubParameters.map(param => `${param}@s.whatsapp.net`);
@@ -14,7 +14,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
   let user = global.db.data.users[who];
   let userName = user ? user.name : await conn.getName(who);
 
-  const thumbnail = await (await fetch('https://h.uguu.se/cYQTFYDL.jpg')).buffer();
+  const thumbnail = await (await fetch('https://files.catbox.moe/73jhb8.jpg')).buffer();
   const redes = 'https://chat.whatsapp.com/tu-grupo'; // Ajustá si querés un link real
 
   if (chat.welcome && m.messageStubType === 27) {
@@ -30,7 +30,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
         isForwarded: true,
         mentionedJid: getMentionedJid(),
         externalAdReply: {
-          title: `😒 Bienvenido/a ${userName} 👋`,
+          title: `😔 Bienvenido/a ${userName} 👋`,
           body: `¡Nos alegra tenerte aquí en *${groupMetadata.subject}*!`,
           previewType: "PHOTO",
           thumbnail,
@@ -72,4 +72,4 @@ export async function before(m, { conn, participants, groupMetadata }) {
       fileName: `despedida.mp3`
     }, { quoted: fkontak, ephemeralExpiration: 24 * 60 * 100, disappearingMessagesInChat: 24 * 60 * 100 });
   }
-      }
+          }
