922d33349302d45ced8103b7e9c67a7b97eb185f
yosue891 <yosueortega630@gmail.com>
2025-05-29 17:24:43 -0400

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,14 +1,29 @@
-export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
-  if (m.isBaileys && m.fromMe) return !0;
-  if (m.isGroup) return !1;
-  if (!m.message) return !0;
-  if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('jadibot')) return !0;
-  const chat = global.db.data.chats[m.chat];
+export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
+  if (m.isBaileys && m.fromMe) return!0;
+  if (m.isGroup) return!1;
+  if (!m.message) return!0;
+
+  if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('jadibot')) return!0;
+
   const bot = global.db.data.settings[this.user.jid] || {};
-if (m.chat === '120363416409380841@newsletter') return !0
-  if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(`${emoji} Hola @${m.sender.split`@`[0]}, mi creador a desactivado los comandos en los chats privados el cual serás bloqueado, si quieres usar los comandos del bot te invito a que te unas al grupo principal del bot.\n\n${gp1}`, false, {mentions: [m.sender]});
+
+  if (m.chat === '120363416409380841@newsletter') return!0;
+
+  if (bot.antiPrivate &&!isOwner &&!isROwner) {
+    // 📸 Ruta de la imagen que se enviará
+    const imageUrl = 'https://files.catbox.moe/mn8xr6.webp'; // Reemplázala con la imagen que quieras usar
+
+    // 🔗 Mensaje de alerta con el enlace del grupo
+    const messageText = `😔 *Hola* @${m.sender.split`@`[0]},\n\n🚫 *Mi creador ha desactivado los comandos en chats privados, por lo que serás bloqueado.*\n\n🔗 *Si quieres usar los comandos, únete al grupo principal del bot:* https://chat.whatsapp.com/KqkJwla1aq1LgaPiuFFtEY`;
+
+    await conn.sendMessage(m.chat, {
+      image: { url: imageUrl},
+      caption: messageText,
+      mentions: [m.sender]
+});
+
     await this.updateBlockStatus(m.chat, 'block');
-  }
-  return !1;
+}
+
+  return!1;
 }
